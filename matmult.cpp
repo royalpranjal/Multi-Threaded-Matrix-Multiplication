363d32c2693f35c43366706a033082c5a6568a7e
conniebc <70911724+conniebc@users.noreply.github.com>
2022-04-03 15:32:45 -0500

    New changes

--- matmult.cpp
+++ matmult.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <fstream>
 #include <vector>
@@ -16,7 +17,7 @@ vector< vector<int> > mat2;
 vector< vector<int> > mat3;
 
 
-int curr_i,curr_j = 0;
+int curr_i, curr_j = 0;
 
 int _m, _n, _p = 0;
 
@@ -24,167 +25,205 @@ int _m, _n, _p = 0;
 pthread_mutex_t lock;
 
 /* POOL OF THREADS*/
-pthread_t *threads;
+pthread_t* threads;
 
 /* Whether the thread is created or terminated*/
-int *threadstatus;
+int* threadstatus;
 
 /* Each thread computes a single element in matrix*/
-void* compute(int id){
-	while(true){
+void* compute(int id) {
+	while (true) {
 		int loc_i, loc_j;
 
 		pthread_mutex_lock(&lock); //So that data will not get intermixed with multiple threads.
 		loc_i = curr_i;
 		loc_j = curr_j;
-		if(curr_i < _m){
+		if (curr_i < _m) {
 			curr_j++;
-			if(curr_j >= _p){
+			if (curr_j >= _p) {
 				curr_i++;
 				curr_j = 0;
 			}
 		}
 		pthread_mutex_unlock(&lock);
-		if(loc_i < _m){
+		if (loc_i < _m) {
 			mat3[loc_i][loc_j] = 0;
-			for(int i = 0;i < _n;i++){
+			for (int i = 0; i < _n; i++) {
 				mat3[loc_i][loc_j] += mat1[loc_i][i] * mat2[i][loc_j];
 			}
 		}
-		if(threadstatus[id-1] == 0){
+		if (threadstatus[id - 1] == 0) {
 			pthread_exit(0);
 		}
 	}
 }
 
+/*void matrixInput(ifstream file, vector< vector<int> > &matrix, int m, int n) {
+	int entry;
+	int i = 0;
+	int j = 0;
+
+	// Taking input of Matrices from param files for Matrix
+	while (1) {
+		file >> entry;
+		matrix[i][j] = entry;
+		j++;
+		if (j >= n) {
+			i++;
+			j = 0;
+		}
+		if (i >= m)
+			break;
+	}
+}
+
+void displayMatrix(vector< vector<int> > &matrix, int m, int n) {
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << setw(5) << matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+*/
+
 /*  Main Function  */
-int main(){
-	int m, n, p, num;
-	num = 0;
+int main(int argc,char** argv) {
+	int m, n, p, no;
+  stringstream mval(argv[1]);
+  mval >> m;
+  stringstream nval(argv[2]);
+  nval >> n;
+	stringstream pval(argv[3]);
+  pval >> p;
+  stringstream noval(argv[4]);
+  noval >> no;
+	string in1 = argv[5];
+	string in2 = argv[6];
+	string out1 = argv[7];
+	int num = 0;
 
-	while(true){
-		int no, i, j, entry;
-		m = n = p = i = j = 0;
-
-		string in1, in2, out1;
-
+	while (true) {
+     int no, i, j, entry;
+        m = n = p = i = j = 0;
+        
 		/* Taking input of Matrices from user */
 		cout << "\nEnter M, N, P where we multiply matrix A [MxN] & matrix B [NxP]" << endl;
 		cout << "Enter M : ";
 		cin >> m;
-        cout << "Enter N : ";
+		cout << "Enter N : ";
 		cin >> n;
 		cout << "Enter P : ";
 		cin >> p;
 		cout << "Enter the number of threads : ";
 		cin >> no;
-		
-		while(m <= 0 || n <= 0 || p <= 0 || no <= 0) {
-			if(m <= 0){
+
+
+		while (m <= 0 || n <= 0 || p <= 0 || no <= 0) {
+			if (m <= 0) {
 				cout << "Integer should be greater than 0";
 				cout << "Enter M : ";
 				cin >> m;
 			}
-			if(n <= 0){
+			if (n <= 0) {
 				cout << "Integer should be greater than 0";
 				cout << "Enter N : ";
 				cin >> n;
 			}
-			if(p <= 0){
+			if (p <= 0) {
 				cout << "Integer should be greater than 0";
 				cout << "Enter P : ";
 				cin >> p;
 			}
-			if(no <= 0){
+			if (no <= 0) {
 				cout << "Integer should be greater than 0";
 				cout << "Enter the number of threads : ";
 				cin >> no;
 			}
 		}
 
-		cout<< "\nEnter name of input file for Matrix A: ";
+		cout << "\nEnter name of input file for Matrix A: ";
 		cin >> in1;
-		
+
 		// Allocating rows
 		mat1.resize(m);
 		mat2.resize(n);
 		mat3.resize(m);
 
 		// Allocating columns
-		for(int i = 0;i < m; i++){
+		for (int i = 0; i < m; i++) {
 			mat1[i].resize(n);
 			mat3[i].resize(p);
 		}
 
-		for(int i = 0;i < n;i++){
+		for (int i = 0; i < n; i++) {
 			mat2[i].resize(p);
 		}
 
-        // Opening the input file
+		// Opening the input file
 		ifstream file;
 		file.open(in1.c_str());
-		
-		//***Error checking if the file exists***
-		if(!file.is_open()){
-			cout << "Cannot open file\n";
-			return;
-		}
+
+		//matrixInput(file, mat1, m, n);
+
 		i = 0;
 		j = 0;
 
-		/* Taking input of Matrices from files for Matrix A */
-		while (1){
+		///* Taking input of Matrices from files for Matrix A */
+		while (1) {
 			file >> entry;
 			mat1[i][j] = entry;
 			j++;
-			if(j >= n){
+			if (j >= n) {
 				i++;
 				j = 0;
-			}	
-			if(i >= m)
+			}
+			if (i >= m)
 				break;
 		}
 
 		cout << "\nMatrix A of order " << m << " x" << n << " is : \n";
 
-		for(i = 0; i < m; i++){
-			for(j = 0; j < n; j++){
+		//displayMatrix(mat1, m, n);
+
+		for (i = 0; i < m; i++) {
+			for (j = 0; j < n; j++) {
 				cout << setw(5) << mat1[i][j] << " ";
 			}
 			cout << endl;
 		}
-		file.close();
 
+		file.close();
 
 		cout << "\nEnter name of input file for Matrix B: ";
 		cin >> in2;
 
 		file.open(in2.c_str());
-		
-		if(!file.is_open()){
-			cout << "Cannot open the file\n";
-		}	return;
 
-	        i = 0;
+		//matrixInput(file, mat2, n, p);
+		i = 0;
 		j = 0;
 
-		/* Taking input of Matrices from files for Matrix B */
-		while (1){
+		///* Taking input of Matrices from files for Matrix B */
+		while (1) {
 			file >> entry;
 			mat2[i][j] = entry;
 			j++;
-			if(j >= p){
+			if (j >= p) {
 				i++;
 				j = 0;
 			}
-			if(i >= n){
+			if (i >= n) {
 				break;
 			}
 		}
 
 		cout << "\nMatrix B of order " << n << " x" << p << " is : \n";
-		for(i = 0; i < n; i++){
-			for(j = 0; j < p; j++){
+
+		//displayMatrix(mat2, n, p);
+
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < p; j++) {
 				cout << setw(5) << mat2[i][j] << " ";
 			}
 			cout << endl;
@@ -195,13 +234,13 @@ int main(){
 
 		//to prevent intermixing of data
 		pthread_mutex_lock(&lock);
-		if(num > no){
-			for(;num > no; num--){
-                // deleting threads which are extra
-				threadstatus[num-1] = 0;
-				cout<<"Thread "<<num<<" terminated\n";
+		if (num > no) {
+			for (; num > no; num--) {
+				// deleting threads which are extra
+				threadstatus[num - 1] = 0;
+				cout << "Thread " << num << " terminated\n";
 			}
-			threads = (pthread_t *) realloc(threads, sizeof(pthread_t) * num);
+			threads = (pthread_t*)realloc(threads, sizeof(pthread_t) * num);
 			cout << "\nThere are " << num << " threads in the thread pool.\n\n";
 		}
 
@@ -213,52 +252,33 @@ int main(){
 
 		pthread_mutex_unlock(&lock);
 
-        // Starting the clock to note execution time
-        double duration;
-        clock_t start;
-        start = clock();
-
 		//Creating threads
-		if(no > num){
-			threads = (pthread_t *) realloc(threads, sizeof(pthread_t) * no);
-			threadstatus = (int *) realloc(threadstatus, sizeof(int) * no);
-			for(;num < no; num++){
+		if (no > num) {
+			threads = (pthread_t*)realloc(threads, sizeof(pthread_t) * no);
+			threadstatus = (int*)realloc(threadstatus, sizeof(int) * no);
+			for (; num < no; num++) {
 				threadstatus[num] = 1;
-				pthread_create(&threads[num], NULL, (void *(*) (void *)) compute, (void *) (num + 1));
+				pthread_create(&threads[num], NULL, (void* (*) (void*)) compute, (void*)(num + 1));
 				cout << "Thread " << (num + 1) << " created\n";
 			}
-			cout << "\nThere are " << num <<" threads in the thread pool.\n\n";
+			cout << "\nThere are " << num << " threads in the thread pool.\n\n";
 		}
 
-		cout << "Enter name of output file : ";
-		cin >> out1;
-
-		/***OPENING OUTPUT FILE***/
-		ofstream file2;
-		file2.open(out1.c_str());
-
-
+    cout << "Enter name of output file : ";
+    cin >> out1;
+        
+		ofstream outfile;
+		outfile.open(out1.c_str());
 		/*Final thread that is main function gives final output*/
 		// bounded wait
-		while(curr_i <_m);
-       		file2 << "\nThe resultant matrix is - \n";
-		for(int i = 0;i < m;i++){
-			for(int j = 0;j < p;j++){
-				file2<<mat3[i][j]<<"\t";
+		while (curr_i < _m);
+		cout << "\nThe resultant matrix is - \n";
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < p; j++) {
+				outfile << mat3[i][j] << "\t";
 			}
-			file2<<"\n";
+			outfile << "\n";
 		}
-
-		// printing the time
-        cout << "time: " << (clock()-start)/double(CLOCKS_PER_SEC) << endl;
-
-		cout << "\nIf u wantto continue, press 'y'. Else press 'n': \n";
-		cin >> in1;
-		if(in1 != "y"){
-			break;
-		}
-		file2.close();
-		/***CLOSING OUTPUT FILE***/
 	}
 
 	return 0;
